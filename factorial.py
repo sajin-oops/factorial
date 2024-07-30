6a553734576b4978345321c33cf21646abaabc0c
A.K.SAJIN <108419506+sajin-oops@users.noreply.github.com>
2024-07-30 18:46:43 +0530

    Add files via upload

---
+++ factorial.py
@@ -0,0 +1,16 @@
+# Factorial
+n = 5
+fact = 1
+
+for i in range(1, n+1):
+    fact = fact * i
+
+print("The factorial of 5 is : ", end="")
+print(fact)
+'''
+1*1 = 1
+1*2 = 2
+1*3 2*3 =6
+1*4 6*4 = 24
+1*5 24*5 = 120
+'''
